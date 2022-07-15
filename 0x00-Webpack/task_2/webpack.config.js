d049bc7cf2b3f7603b85a2851eec90b404a92102
Qimae <kartelbwoy@gmail.com>
2022-07-15 13:32:40 +0300

    task 2

--- 0x00-Webpack/task_2/webpack.config.js
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -1,40 +1,39 @@
 const path = require("path");
 
 module.exports = {
-  mode: 'production',
-  entry: {
-    main: path.resolve(__dirname, './js/dashboard_main.js'),
-  },
-  output: {
-    path: path.resolve(__dirname, 'public'),
-    filename: 'bundle.js',
-  },
-  performance: {
-		maxAssetSize: 1000000,
-    maxEntrypointSize: 1000000,
+	mode: 'production',
+	entry: {
+		main: path.resolve(__dirname, './js/dashboard_main.js'),
 	},
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"]
-      },
+	output: {
+		path: path.resolve(__dirname, 'public'),
+		filename: 'bundle.js',
+	},
+	performance: {
+		maxAssetSize: 1000000,
+		maxEntrypointSize: 1000000,
+	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/,
+				use: ["style-loader", "css-loader"]
+			},
 
-      {
-        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
-        type: 'asset/resource',
-        use: ['file-loader',
-          {
-            loader: 'image-webpack-loader',
-            options: {
-              bypassingOnDebug: true,
-              disable: true,
-              import: true,
-            },
-          },
-        ],
+			{
+				test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+				type: 'asset/resource',
+				use: ['file-loader',
+					{
+						loader: 'image-webpack-loader',
+						options: {
+							bypassingOnDebug: true,
+							disable: true,
+						},
+					},
+				],
 
-      },
-    ]
-  },
+			},
+		]
+	},
 };
