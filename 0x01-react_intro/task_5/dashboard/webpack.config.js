cd3110354be41b6656095a9ff1a0da1498448b8b
Qimae <kartelbwoy@gmail.com>
2022-07-16 13:46:05 +0300

    webpack config fix

--- 0x01-react_intro/task_5/dashboard/webpack.config.js
+++ 0x01-react_intro/task_5/dashboard/webpack.config.js
@@ -1,12 +1,44 @@
 const path = require("path");
 
 module.exports = {
-  mode: 'production',
-  entry: {
-    main: path.resolve(__dirname, './src/index.js'),
-  },
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-  },
+    mode: 'development',
+    devtool: "inline-source-map",
+    entry: {
+        main: path.resolve(__dirname, './src/index.js'),
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+    },
+    devServer: {
+        hot: true,
+        contentBase: path.resolve("./dist"),
+        compress: true,
+        port: 8564,
+    },
+    performance: {
+        maxAssetSize: 1000000,
+        maxEntrypointSize: 1000000,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.(gif|png|jpe?g|svg)$/i,
+                use: [
+                    "file-loader",
+                    {
+                        loader: "image-webpack-loader",
+                        options: {
+                            bypassOnDebug: true,
+                            disable: true,
+                        },
+                    },
+                ],
+            },
+        ],
+    },
 };
