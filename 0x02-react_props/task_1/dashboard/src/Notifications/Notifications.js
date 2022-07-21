24ddaaaf9f66980e6d5b3e5052593e0d66d77479
Qimae <kartelbwoy@gmail.com>
2022-07-21 15:24:03 +0300

    tests

---
+++ 0x02-react_props/task_1/dashboard/src/Notifications/Notifications.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Notifications.css';
+import { getLatestNotification } from '../utils/utils';
+import close from '../assets/close-icon.png';
+
+
+export default function Notifications() {
+    return (
+        <div className='Notifications'>
+            <p>Here is the list of notifications</p>
+            <button style={{ position: 'absolute', top: '7px', right: '7px', border:'none', background: 'none', padding: '0' }} aria-label='close' onClick={console.log('Close button has been clicked')} >
+                <img src={close} alt='close'></img>
+            </button>
+            <ul>
+                <li data='default'>New course available</li>
+                <li data='urgent'>New resume available</li>
+                <li data='urgent' dangerouslySetInnerHTML={{ __html: getLatestNotification() }}></li>
+            </ul>
+        </div>
+    );
+}
