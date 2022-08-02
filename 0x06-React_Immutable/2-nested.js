7ca70bb04f4b917820caa41a0e0b527785e251f4
Qimae <kartelbwoy@gmail.com>
2022-08-02 10:07:59 +0300

    Immutable

---
+++ 0x06-React_Immutable/2-nested.js
@@ -0,0 +1,7 @@
+import { fromJS } from "./node_modules/immutable/dist/immutable";
+
+export default function accessImmutableObject(object, array) {
+    const mappedObj = fromJS(object);
+
+    return mappedObj.getIn(array, undefined);
+}
